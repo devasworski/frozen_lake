c2d080fa66e2a6370cc5ef0272c12e03afeca96c
Alexander Sworski <mail@dev-sworski.com>
2022-01-03 22:45:29 +0100

    format model_based_rl.py

--- model_based_rl.py
+++ model_based_rl.py
@@ -4,7 +4,7 @@ import numpy as np
 
 ### policy iteration section ###
 
-''' policy_evaluation function TODO
+''' policy_evaluation function
     evaluates the given policy
     
     @param env
@@ -25,32 +25,18 @@ def policy_evaluation(env, policy, gamma, theta, max_iterations):
     value = np.zeros(env.n_states, dtype=np.float)
 
     #TODO:
-
-    # Iterate until the max iteration
     for _ in range(max_iterations):
-
         delta = 0
-        for s in range(env.n_states):
-
-            v = value[s]
-
-            # Computing the current value for policy evaluation
-            value[s] = sum(
-                [
-                    env.p(next_s, s, policy[s]) *
-                    (env.r(next_s, s, policy[s]) + gamma * value[next_s])
-                    for next_s in range(env.n_states)
-                ]
-            )
-
-            delta = max(delta, abs(v - value[s]))  # difference to check convergence
-
-        # Breaks when policy converges
+        for state in range(env.n_states):
+            v = value[state]
+            value[state] = sum([env.p(next_s, state, policy[state]) * (env.r(next_s, state, policy[state]) + gamma * value[next_s]) 
+            for next_s in range(env.n_states)])
+            delta = max(delta, abs(v - value[state]))
         if delta < theta:
             break
     return value
 
-''' policy_improvement function TODO
+''' policy_improvement function
     create a policy for each possible game state
     
     @param env
@@ -67,24 +53,14 @@ def policy_improvement(env, value, gamma):
     policy = np.zeros(env.n_states, dtype=int)
 
     #TODO:
-
     for s in range(env.n_states):
-        policy[s] = np.argmax(  # picks action for each state with the highest expected reward
-            [
-                sum(
-                    [
-                        env.p(next_s, s, a) *
-                        (env.r(next_s, s, a) + gamma * value[next_s])
-                        for next_s in range(env.n_states)
-                    ]
-                )
-                for a in range(env.n_actions)
-            ]
-        )
-
+        policy[s] = np.argmax([
+            sum([env.p(next_s, s, a) * (env.r(next_s, s, a) + gamma * value[next_s])
+            for next_s in range(env.n_states)])
+            for a in range(env.n_actions)])
     return policy
 
-''' policy_iteration function TODO
+''' policy_iteration function
     Iteratively improve the policy
     
     @param env
@@ -109,24 +85,19 @@ def policy_iteration(env, gamma, theta, max_iterations, policy=None):
         policy = np.zeros(env.n_states, dtype=int)
     else:
         policy = np.array(policy, dtype=int)
-
     #TODO:
-
     while(True):
         policy_initial = policy
-
         value = policy_evaluation(env, policy, gamma, theta, max_iterations)
         policy = policy_improvement(env, value, gamma)
-
         if np.array_equal(policy_initial, policy):
             break
-
     return policy, value
 
 
 ### value iteration section ###
 
-''' value_iteration function TODO
+''' value_iteration function
     iteralively increased the value and uses the value to create the policy
     
     @param env
@@ -153,43 +124,23 @@ def value_iteration(env, gamma, theta, max_iterations, value=None):
         value = np.array(value, dtype=np.float)
 
     #TODO:
-
     for _ in range(max_iterations):
         delta = 0
-
-        for s in range(env.n_states):
-            v = value[s]
-            value[s] = max(
-                [
-                    sum(
-                        [
-                            env.p(s_next, s, a) *
-                            (env.r(s_next, s, a) + gamma * value[s_next])
-                            for s_next in range(env.n_states)
-                        ]
-                    )
-                    for a in range(env.n_actions)
-                ]
-            )
-
-            delta = max(delta, abs(v - value[s]))
-
+        for state in range(env.n_states):
+            v = value[state]
+            value[state] = max([
+            sum([env.p(s_next, state, action) * (env.r(s_next, state, action) + gamma * value[s_next]) 
+            for s_next in range(env.n_states)]) 
+            for action in range(env.n_actions)])
+            delta = max(delta, abs(v - value[state]))
         if delta < theta:
             break
 
     policy = np.zeros(env.n_states, dtype=int)
-    for s in range(env.n_states):
-        policy[s] = np.argmax(
-            [
-                sum(
-                    [
-                        env.p(s_next, s, a) *
-                        (env.r(s_next, s, a) + gamma * value[s_next])
-                        for s_next in range(env.n_states)
-                    ]
-                )
-                for a in range(env.n_actions)
-            ]
-        )
+    for state in range(env.n_states):
+        policy[state] = np.argmax(
+        [sum([env.p(s_next, state, action) * (env.r(s_next, state, action) + gamma * value[s_next])
+        for s_next in range(env.n_states)])
+        for action in range(env.n_actions)])
 
     return policy, value
